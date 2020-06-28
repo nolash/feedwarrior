a873ca0a3cd8165ab165cff36fb2ab2a59114b28
nolash <dev@holbrook.no>
2020-06-28 22:28:31 +0200

    Add simple summary output on show

--- src/feedwarrior/cmd/show.py
+++ src/feedwarrior/cmd/show.py
@@ -1,3 +1,8 @@
+# standard imports
+import json
+import email
+import time
+
 def parse_args(argparser):
     pass
 
@@ -5,9 +10,41 @@ def check_args(args):
     pass
 
 def execute(config, feed, args):
+    i = 0
     while 1:
         try:
-            print(feed.next_entry())
+            e = feed.next_entry()
         except IndexError:
             break
+        j = json.loads(e)
+        m = email.message_from_string(j['payload'])
+        tf = email.utils.parsedate(m.get('Date'))
+        t = int(time.mktime(tf))
+        tl = time.localtime(t)
+        ts = time.strftime('%x %X', tl)
+        body = None
+        attachments = []
+        ii = 0
+        for p in m.walk():
+            ii += 1
+            if ii == 1:
+                continue
+
+            if 'attachment' in p.get_content_disposition():
+                attachments.append('{} ({})'.format(p.get_filename(), p.get_content_type()))
+            elif p.get_content_maintype() == 'text':
+                if p.get_filename() == '_content' or body == None:
+                    body = p.get_payload(decode=True).decode('utf-8')
+
+        if i > 0:
+           print('----')
+
+
+        if body != None:
+            print('{} - {}'.format(ts, j['uuid']))
+            print(body)
+            for a in attachments:
+                print('+ {}'.format(a))
+
+        i += 1
     pass
