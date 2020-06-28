303a9736cfa8958e69f3c0231ded02b2ebb90805
nolash <dev@holbrook.no>
2020-06-28 22:57:03 +0200

    Update setup.py with new package path

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='feedwarrior',
-    version='0.1.0',
+    version='0.1.2',
     description='feeds, warrior style',
     author='Louis Holbrook',
     author_email='dev@holbrook.no',
-    packages=['src/feedwarrior', 'src/feedwarrior.cmd'],
+    packages=['feedwarrior', 'feedwarrior.cmd'],
     install_requires=['xdg'],
     scripts = [
         'scripts/feedwarrior',
