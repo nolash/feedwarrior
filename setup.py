104f0462128d96eb9ff7686229512240512e5759
nolash <dev@holbrook.no>
2020-06-30 00:30:51 +0200

    Rename -r flag to --headers

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='feedwarrior',
-    version='0.1.4',
+    version='0.2.0',
     description='feeds, warrior style',
     author='Louis Holbrook',
     author_email='dev@holbrook.no',
