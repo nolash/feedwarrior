e238e391c4ebe6150f3e4dca3b0820a023778265
nolash <dev@holbrook.no>
2020-08-28 00:21:05 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='feedwarrior',
-    version='0.4.1',
+    version='0.4.2',
     description='feeds, warrior style',
     author='Louis Holbrook',
     author_email='dev@holbrook.no',
