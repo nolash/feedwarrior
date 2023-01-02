6329267d351870139992722bb0b87232e22575a5
lash <dev@holbrook.no>
2023-01-02 09:23:36 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ f.close()
 
 setup(
     name='feedwarrior',
-    version='0.5.2',
+    version='0.5.3',
     description='feeds, warrior style',
     author='Louis Holbrook',
     author_email='dev@holbrook.no',
